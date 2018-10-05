c6c0d294cf5f24a5645680bd40925e0d9fe3b48f
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-10-05 23:09:01 +0000

    fixing #run and writing stub for #display_apps

--- lib/top_apps/cli.rb
+++ lib/top_apps/cli.rb
@@ -1,7 +1,7 @@
 class TopApps::CLI
   def run
     index_array = TopApps::Scrape.scrape_index("https://www.apple.com/itunes/charts/")
-    TopApps::App.create_apps_from_index(array)
+    TopApps::App.create_apps_from_index(index_array)
     greeting
     display_apps
     end
@@ -10,11 +10,15 @@ class TopApps::CLI
       puts "Welcome to Top Apps!"
       puts "Top Apps is a CLI that displays the hottest apps in the Apple Store right now."
       puts "You can select an app to learn more about it."
+      puts "\n"
     end
 
     def display_apps
       puts "Here are the top 5 free apps in the Apple Store right now:"
       #iterate over App.all and puts app information, prompt user to select an app by entering a number
+      TopApps::App.all.each do |app|
+        puts "#{app.rank}. #{app.name} - #{app.category}"
+      end
     end
 
     def display_profile
