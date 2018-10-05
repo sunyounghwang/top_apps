f3d7ad40bc73046fbd762c54b4303d506b387353
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-10-05 22:43:34 +0000

    completing #scrape_index

--- lib/top_apps/scrape.rb
+++ lib/top_apps/scrape.rb
@@ -2,16 +2,33 @@ class TopApps::Scrape
   require "open-uri"
 =begin
   index_url = "https://www.apple.com/itunes/charts/"
-  index_array = {
-    name:
-    category:
-    rank:
-    profile_url:
+  apps_section = doc.css(".section.chart-grid.apps div.section-content")
+  free_apps_section = doc.css(".section.chart-grid.apps div.section-content").first
+  free_apps_list = free_apps_section.css("li")
+
+  index_hash = {
+    name: app.css("h3 a").text,
+    category: app.css("h4 a").text,
+    rank: app.css("strong").text.chomp("."),
+    profile_url: app.css("a").attribute("href").value
   }
 =end
 
   def self.scrape_index(index_url)
+    index_array = []
     doc = Nokogiri::HTML(open(index_url))
+    free_apps_section = doc.css(".section.chart-grid.apps div.section-content").first
+    free_apps_list = free_apps_section.css("li")
+
+    free_apps_list.each do |app|
+      index_hash = {
+        name: app.css("h3 a").text,
+        category: app.css("h4 a").text,
+        rank: app.css("strong").text.chomp("."),
+        profile_url: app.css("a").attribute("href").value
+      }
+      index_array << index_hash
+    end
   end
 
   def self.scrape_profile(profile_url)
