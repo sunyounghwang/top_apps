eb4bb013e0f85884f794e854577c65fe2d22b6ae
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-10-05 22:55:32 +0000

    adding App #all

--- lib/top_apps/app.rb
+++ lib/top_apps/app.rb
@@ -2,13 +2,17 @@ class TopApps::App
  @@all = []
  attr_accessor :name, :category, :rank, :profile_url
 
+ def self.create_apps_from_index(index_array)  #index array returned by #scrape_index
+   index_array.each { |index_hash| self.new(index_hash) }
+ end
+
+ def self.all
+   @@all
+ end
+
  def initialize(hash)
    hash.each { |key, value| self.send("#{key}=", value) }
    @@all << self
- end
-
- def self.create_apps_from_index(index_array)  #index array returned by #scrape_index
-   index_array.each { |index_hash| self.new(index_hash) }
  end
 
  def add_attributes_from_profile(profile_hash)  #profile hash returned by #scrape_profile
