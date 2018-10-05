8901099706b7060f04d8699372558c3dbb78fb1e
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-10-05 23:18:25 +0000

    fixing CLI indentation and adding #create_apps

--- lib/top_apps/cli.rb
+++ lib/top_apps/cli.rb
@@ -1,28 +1,32 @@
 class TopApps::CLI
   def run
-    index_array = TopApps::Scrape.scrape_index("https://www.apple.com/itunes/charts/")
-    TopApps::App.create_apps_from_index(index_array)
+    create_apps
     greeting
     display_apps
-    end
+  end
 
-    def greeting
-      puts "Welcome to Top Apps!"
-      puts "Top Apps is a CLI that displays the hottest apps in the Apple Store right now."
-      puts "You can select an app to learn more about it."
-      puts "\n"
-    end
+  def create_apps
+    index_array = TopApps::Scrape.scrape_index("https://www.apple.com/itunes/charts/")
+    TopApps::App.create_apps_from_index(index_array)
+  end
 
-    def display_apps
-      puts "Here are the top 5 free apps in the Apple Store right now:"
-      #iterate over App.all and puts app information, prompt user to select an app by entering a number
-      TopApps::App.all.each do |app|
-        puts "#{app.rank}. #{app.name} - #{app.category}"
-      end
-    end
+  def greeting
+    puts "Welcome to Top Apps!"
+    puts "Top Apps is a CLI that displays the hottest apps in the Apple Store right now."
+    puts "You can select an app to learn more about it."
+    puts "\n"
+  end
 
-    def display_profile
-      #find selected app in App.all and puts selected app's profile, prompt user to enter "back" or "exit"
+  def display_apps
+    puts "Here are the top 5 free apps in the Apple Store right now:"
+    #iterate over App.all and puts app information, prompt user to select an app by entering a number
+    TopApps::App.all.each do |app|
+      puts "#{app.rank}. #{app.name} - #{app.category}"
     end
+  end
+
+  def display_profile
+    #find selected app in App.all and puts selected app's profile, prompt user to enter "back" or "exit"
+  end
 
 end
