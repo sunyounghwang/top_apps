6d3dce5ddcd0563638b56a3f28c87a0d7ecd1f22
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-10-05 19:48:45 +0000

    adding nokogiri, open-uri, pry requirements

--- lib/top_apps.rb
+++ lib/top_apps.rb
@@ -1,4 +1,7 @@
 module TopApps
 end
 
+require "nokogiri"
+require "open-uri"
+require "pry"
 require_relative "top_apps/cli"
--- lib/top_apps/app.rb
+++ lib/top_apps/app.rb
@@ -7,11 +7,11 @@ class TopApps::App
    @@all << self
  end
 
- def create_apps_from_index(index_array)
+ def create_apps_from_index(index_array)  #index array returned by #scrape_index
    index_array.each { |index_hash| self.new(index_hash) }
  end
 
- def add_attributes_from_profile(profile_hash)
+ def add_attributes_from_profile(profile_hash)  #profile hash returned by #scrape_profile
    profile_hash.each { |profile_hash| self.send("#{key}=", value) }
  end
 
