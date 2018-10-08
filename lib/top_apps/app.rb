9c2d191c1d2232ac22467afaffbfbb29bbe2aaad
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-10-08 21:43:56 +0000

    removing self from App methods

--- lib/top_apps/app.rb
+++ lib/top_apps/app.rb
@@ -3,7 +3,7 @@ class TopApps::App
  attr_accessor :name, :category, :rank, :profile_url, :notes, :developer, :rating
 
  def self.create_apps_from_index(index_array)
-   index_array.each { |index_hash| self.new(index_hash) }
+   index_array.each { |index_hash| new(index_hash) }
  end
 
  def self.all
@@ -11,12 +11,12 @@ class TopApps::App
  end
 
  def initialize(hash)
-   hash.each { |key, value| self.send("#{key}=", value) }
+   hash.each { |key, value| send("#{key}=", value) }
    @@all << self
  end
 
- def add_attributes_from_profile(profile_hash)  
-   profile_hash.each { |key, value| self.send("#{key}=", value) }
+ def add_attributes_from_profile(profile_hash)
+   profile_hash.each { |key, value| send("#{key}=", value) }
  end
 
 end
